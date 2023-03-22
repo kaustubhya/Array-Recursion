ba5e4ee4d76c865bc4fa59c2e489534df1ab9ac5
kaustubhya <kaustubhyaksd@gmail.com>
2023-03-22 13:06:50 +0530

    here!

--- Rotated_Binary_Search.java
+++ Rotated_Binary_Search.java
@@ -1,7 +1,7 @@
 public class Rotated_Binary_Search {
     public static void main(String[] args) {
         int [] array = {5,6,7,8,9,1,2,3};
-        int ans = search(array, 66, 0, array.length -1);
+        int ans = search(array, 9, 0, array.length -1);
         System.out.println(ans);
         
     }
